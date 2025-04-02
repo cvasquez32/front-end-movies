9fdc7d0f8574be2e2d154416e2b9b61e03a7ef99
carlos vasquez <cvasquez0100@gmail.com>
2025-04-01 22:12:25 -0500

    fetch movies from api

--- src/App.js
+++ src/App.js
@@ -1,8 +1,34 @@
+import { useState, useEffect } from "react";
 import "./App.css";
 
 function App() {
+  const [movie, setMovie] = useState([]);
+  useEffect(() => {
+    movies();
+  });
+
+  const movies = async () => {
+    const response = await fetch("http://localhost:3001/api/v1/movie/");
+    setMovie(await response.json());
+  };
   return (
-    <h1 className="text-3xl font-bold underline text-center p-4">Movies</h1>
+    <div>
+      <h1 className="text-3xl font-bold underline text-center p-4">Movies</h1>
+      <div className="flex justify-center">
+        <ol className="list-decimal pl-6 space-y-2 text-center">
+          {movie.map((data) => {
+            return (
+              <li
+                key={data.id}
+                className="text-gray-800 hover:text-blue-600 transition-colors duration-200"
+              >
+                {data.title}
+              </li>
+            );
+          })}
+        </ol>
+      </div>
+    </div>
   );
 }
 
