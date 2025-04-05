67418d3741b8e9d8b4a836950e6de01ca8b9df7d
carlos vasquez <cvasquez0100@gmail.com>
2025-04-05 17:37:19 -0500

    start work for post call on form component

--- src/components/MovieForm.js
+++ src/components/MovieForm.js
@@ -1,10 +1,34 @@
-import React from 'react';
+import { useState, useEffect } from "react";
 
 const MovieForm = () => {
+  // const [movie, setMovie] = useState({
+  //   title: "",
+  //   date: "",
+  // });
+
+  // const [error, SetError] = useState(false);
+
+  // useEffect(() => {
+  //   const requestOptions = {
+  //     method: "POST",
+  //     headers: { "Content-Type": "application/json" },
+  //     body: JSON.stringify(movie),
+  //   };
+  //   fetch("http://localhost:3000/api/v1/movie", requestOptions)
+  //     .then((response) => response.json())
+  //     .then((data) => setMovie(data));
+  // }, []);
+
   return (
-    <form className="max-w-md mx-auto p-6 bg-white shadow rounded" method="POST">
+    <form
+      className="max-w-md mx-auto p-6 bg-white shadow rounded"
+      method="POST"
+    >
       <div className="mb-4">
-        <label htmlFor="title" className="block text-gray-700 font-semibold mb-2">
+        <label
+          htmlFor="title"
+          className="block text-gray-700 font-semibold mb-2"
+        >
           Movie Title
         </label>
         <input
@@ -16,7 +40,10 @@ const MovieForm = () => {
         />
       </div>
       <div className="mb-4">
-        <label htmlFor="date" className="block text-gray-700 font-semibold mb-2">
+        <label
+          htmlFor="date"
+          className="block text-gray-700 font-semibold mb-2"
+        >
           Release Date
         </label>
         <input
